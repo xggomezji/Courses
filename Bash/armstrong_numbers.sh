54bf548bc32601bf67a09fe4d8ea025209478c5e
xggomezji <128899331+xggomezji@users.noreply.github.com>
2023-07-06 11:00:41 +0200

    Create armstrong_numbers.sh

---
+++ Bash/armstrong_numbers.sh
@@ -0,0 +1,24 @@
+'''
+Instructions
+An Armstrong number is a number that is the sum of its own digits each raised to the power of the number of digits.
+
+For example:
+
+9 is an Armstrong number, because 9 = 9^1 = 9
+10 is not an Armstrong number, because 10 != 1^2 + 0^2 = 1
+153 is an Armstrong number, because: 153 = 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+154 is not an Armstrong number, because: 154 != 1^3 + 5^3 + 4^3 = 1 + 125 + 64 = 190
+Write some code to determine whether a number is an Armstrong number.
+'''
+#!/usr/bin/env bash
+Armstrong_number() {
+    AUX=0
+    for ((i=0;i<${#1}; i++)); do
+        AUX=$((AUX+(( ${1:i:1} ** ${#1} ))))
+    done
+    if [ $1 -eq $AUX ]; then
+        echo "true"
+    else
+        echo "false"
+    fi
+}
